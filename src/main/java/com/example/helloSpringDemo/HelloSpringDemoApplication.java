a0082e0f132ae92075c55bcc7c98181bd8c088d4
imfarhad <fakhrul.farhad@gmail.com>
2021-09-03 22:45:18 +0300

    first commit

---
+++ src/main/java/com/example/helloSpringDemo/HelloSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.helloSpringDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/helloSpringDemo/HelloSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helloSpringDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
